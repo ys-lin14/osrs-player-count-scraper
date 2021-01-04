8bd65bedc31b3adfb59fe0cb702cea34b7123406
Derek Lin <ys.lin113@gmail.com>
2021-01-04 15:54:35 -0800

    replace etl functions

---
+++ 02-scrape-world-select.py
@@ -0,0 +1,36 @@
+import datetime
+
+from log import check_last_request, update_logs
+from extract import get_osrs_world_select, extract_data
+from transform import transform_data
+from load import load_data
+
+def scrape_world_select():
+    dt = datetime.now()
+    response = get_osrs_world_select()
+    status_code = response.status_code
+
+    if (response.ok):
+        world_data, total_player_data = extract_data(response)
+        
+        world_data, total_player_count = (
+            transform_data(world_data, total_player_data, dt)
+        )
+
+        load_data(world_data, total_player_count)
+    else:
+        print('Bad Response - HTTP', status_code)
+
+    update_logs(dt, status_code)
+
+
+def main():
+    logs = pd.read_csv('data/logs.csv')
+    last_request_successful = check_last_request(logs)
+
+    if (last_request_successful):
+        scrape_world_select()
+
+
+if __name__ == '__main__':
+    main()
