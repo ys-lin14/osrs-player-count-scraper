aa338868e0c6b32208915e775596ee08a103adbf
Derek Lin <ys.lin113@gmail.com>
2021-01-03 22:19:58 -0800

    move extract functions from etl_prototype to python file

---
+++ extract.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+import requests
+
+from bs4 import BeautifulSoup
+
+
+def get_osrs_world_select():    
+    world_select_url = ('https://oldschool.runescape.com/slu')
+    response = requests.get(world_select_url)
+    return response
+
+
+def extract_world_data(response):
+    soup = BeautifulSoup(response.text, 'lxml')
+    data = soup.findAll('tr', {'class':'server-list__row'})
+    data = pd.DataFrame(np.array(data, dtype=object))
+    return data
+
+
+def extract_total_player_data(response):
+    soup = BeautifulSoup(response.text, 'lxml')
+    data = soup.find('p')
+    data = pd.DataFrame(data, columns=['player_count'])
+    return data
