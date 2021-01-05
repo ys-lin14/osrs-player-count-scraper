341197e76e41e728dd0336a43410c90185d6e4e4
Derek Lin <ys.lin113@gmail.com>
2021-01-04 16:23:55 -0800

    replace ...0900_ai... with general collate

--- create-osrs-database.sql
+++ create-osrs-database.sql
@@ -8,10 +8,10 @@ SET character_set_client = utf8mb4;
 CREATE TABLE total_player_counts (
 	`datetime` DATETIME,
     player_count MEDIUMINT UNSIGNED NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 CREATE TABLE world_player_counts (
     `datetime` DATETIME,
 	world SMALLINT UNSIGNED NOT NULL,
     player_count MEDIUMINT UNSIGNED NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
