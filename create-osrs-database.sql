b13d346332101b449faceeef183bd08407ee2d49
Derek Lin <ys.lin113@gmail.com>
2021-01-04 16:02:12 -0800

    add sql that creates the osrs database

---
+++ create-osrs-database.sql
@@ -0,0 +1,17 @@
+DROP DATABASE IF EXISTS osrs;
+CREATE DATABASE osrs;
+USE osrs;
+
+SET NAMES utf8mb4;
+SET character_set_client = utf8mb4;
+
+CREATE TABLE total_player_counts (
+	`datetime` DATETIME,
+    player_count MEDIUMINT UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE world_player_counts (
+    `datetime` DATETIME,
+	world SMALLINT UNSIGNED NOT NULL,
+    player_count MEDIUMINT UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
